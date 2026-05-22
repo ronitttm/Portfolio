ee8ae904574679b524724e9100404c73cc3834db
ronitttm <ronit.murpani@vit.edu.in>
2026-05-22 23:48:04 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -6,17 +6,23 @@ function toggleMenu() {
 }
 
 const projectsCarousel = document.querySelector(".projects-carousel");
+const projectsPrev = document.getElementById("projects-prev");
 const projectsNext = document.getElementById("projects-next");
 let projectsIndex = 0;
 
-function scrollProjects() {
+function scrollProjects(direction = 1) {
   if (!projectsCarousel) return;
   const cards = projectsCarousel.querySelectorAll(".project-card");
   if (!cards.length) return;
-  projectsIndex = (projectsIndex + 1) % cards.length;
+  projectsIndex = (projectsIndex + direction + cards.length) % cards.length;
   cards[projectsIndex].scrollIntoView({ behavior: "smooth", inline: "start", block: "nearest" });
 }
 
-if (projectsNext && projectsCarousel) {
-  projectsNext.addEventListener("click", scrollProjects);
+if (projectsCarousel) {
+  if (projectsPrev) {
+    projectsPrev.addEventListener("click", () => scrollProjects(-1));
+  }
+  if (projectsNext) {
+    projectsNext.addEventListener("click", () => scrollProjects(1));
+  }
 }
