cb4c2b37b65ff8ee048c8d3eae900fa94ef8d98f
ronitttm <ronit.murpani@vit.edu.in>
2026-05-22 23:39:56 +0530

    project carousel added

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const projectsCarousel = document.querySelector(".projects-carousel");
+const projectsNext = document.getElementById("projects-next");
+let projectsIndex = 0;
+let projectsAutoScroll = null;
+
+function scrollProjects() {
+  if (!projectsCarousel) return;
+  const cards = projectsCarousel.querySelectorAll(".project-card");
+  if (!cards.length) return;
+  projectsIndex = (projectsIndex + 1) % cards.length;
+  cards[projectsIndex].scrollIntoView({ behavior: "smooth", inline: "start", block: "nearest" });
+}
+
+function startProjectAutoScroll() {
+  clearInterval(projectsAutoScroll);
+  projectsAutoScroll = setInterval(scrollProjects, 5000);
+}
+
+if (projectsNext && projectsCarousel) {
+  projectsNext.addEventListener("click", () => {
+    scrollProjects();
+    startProjectAutoScroll();
+  });
+
+  projectsCarousel.addEventListener("mouseenter", () => clearInterval(projectsAutoScroll));
+  projectsCarousel.addEventListener("mouseleave", () => startProjectAutoScroll());
+  startProjectAutoScroll();
+}
